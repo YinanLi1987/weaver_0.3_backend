dd59bad68fd3f4a2451b2332a66b8cb7cdb603c4
YinanLi1987 <t1liyi00@students.oamk.fi>
2025-05-20 10:37:51 +0300

    fix cros

--- app/main.py
+++ app/main.py
@@ -17,7 +17,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",  # 本地开发
     "http://localhost:3000",  # serve dist 本地测试
-    os.getenv("FRONTEND_URL", "https://weaver-03.vercel.app")  # 生产站点
+    "https://weaver-03.vercel.app" ,
+    "https://weaver-03-yinanli1987s-projects.vercel.app/"# 生产站点
 ]
 # Allow frontend dev server to talk to backend
 app.add_middleware(
